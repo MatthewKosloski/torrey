56f713ffa4871df921ad0dc003922a38dcbe4e3e
Matthew Kosloski <matthewkosloski@me.com>
2021-01-22 19:00:01 -0600

    added javadoc to TypeChecker

--- src/me/mtk/torrey/Analysis/TypeChecker.java
+++ src/me/mtk/torrey/Analysis/TypeChecker.java
@@ -2,19 +2,30 @@ package me.mtk.torrey.Analysis;
 
 import me.mtk.torrey.AST.Program;
 
-public class TypeChecker 
+public final class TypeChecker 
 {
-    
+    // The program that is to be type checked.
     private Program program;
 
+    /**
+     * Constructs a new type checker, initializing
+     * it with the specified Program ASTNode to be
+     * subsequently type checked.
+     * 
+     * @param program A Program ASTNode.
+     */
     public TypeChecker(Program program)
     {
         this.program = program;
     }
 
+    /**
+     * Initiates the type checking of the program.
+     */
     public void check()
     {
-        final TypeCheckerVisitor visitor = new TypeCheckerVisitor();
-        program.accept(visitor);
+        // Calls the TypeCheckerVisitor's overloaded visit()
+        // method that takes a Program ASTNode as an argument.
+        program.accept(new TypeCheckerVisitor());
     }
 }
