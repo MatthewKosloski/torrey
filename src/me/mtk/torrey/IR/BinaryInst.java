3e4706844ec0c0c3c4733a6476fe4ea864f150bd
Matthew Kosloski <matthewkosloski@me.com>
2021-02-02 23:15:59 -0600

    added javadoc comments, renamed instance vars

--- src/me/mtk/torrey/IR/BinaryInst.java
+++ src/me/mtk/torrey/IR/BinaryInst.java
@@ -1,23 +1,39 @@
 package me.mtk.torrey.IR;
 
+/**
+ * Represents a binary arithmetic IR instruction.
+ */
 public class BinaryInst extends IRInst
 {
-    // the binary operator
+    // The type of binary operator.
     BinaryOpType operator;
 
-    // left and right operands
-    Address left, right;
+    // The first and second operands.
+    Address first, second;
 
-    public BinaryInst(Address addr, BinaryOpType op, Address left, Address right)
+    /**
+     * Instantiates a new binary arithmetic IR instruction.
+     * 
+     * @param addr The address at which the result of the arithmetic is to be stored.
+     * @param op The binary operator of this instruction.
+     * @param first The address at which the first operand is located.
+     * @param second The address at which the second operand is located.
+     */
+    public BinaryInst(Address addr, BinaryOpType op, Address first, Address second)
     {
         super(addr);
         operator = op;
-        this.left = left;
-        this.right = right;
+        this.first = first;
+        this.second = second;
     }
 
+    /**
+     * The string representation of this binary arithmetic instruction.
+     * 
+     * @return The string of this instruction.
+     */
     public String toString()
     {
-        return String.format("%s = %s %s %s", addr, operator, left, right);
+        return String.format("%s = %s %s %s", addr, operator, first, second);
     }
 }
