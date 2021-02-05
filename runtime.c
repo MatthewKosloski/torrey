670f99d9f6529f8d76fcf64db2aa1a69b9ed3930
Matthew Kosloski <matthewkosloski@me.com>
2021-02-04 19:42:38 -0600

    added runtime to print 64-bit integers

---
+++ runtime.c
@@ -0,0 +1,11 @@
+#include "runtime.h"
+
+void print_int(int64_t x)
+{
+  printf("%" PRId64, x);
+}
+
+void print_nl(void)
+{
+  printf("\n");
+}
---
+++ runtime.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+// Print an integer to standard output
+void print_int(int64_t x);
+
+// Print a new line character to standard output
+void print_nl(void);
