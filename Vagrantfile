1c8fcef267af560f59ae215111880f615be44763
Matthew Kosloski <matthewkosloski@me.com>
2021-02-04 19:43:16 -0600

    added vagrant vm to build and test programs

---
+++ Vagrantfile
@@ -0,0 +1,7 @@
+Vagrant.configure("2") do |config|
+
+    # Ubuntu 18.04 LTS
+    config.vm.box = "ubuntu/bionic64"
+
+    config.vm.provision :shell, path: "bootstrap.sh"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+# Update the local APT package index and then upgrade any 
+# upgradeable packages.
+apt-get update -y && apt-get upgrade -y
+
+# Install Java JDK and gcc, g++, and make
+apt-get install -y default-jdk build-essential
