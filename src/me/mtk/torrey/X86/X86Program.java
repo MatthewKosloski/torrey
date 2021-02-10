d03c04319dd4026966015dc54e74df15adbfa5e7
Matthew Kosloski <matthewkosloski@me.com>
2021-02-10 01:16:02 -0600

    very preliminary toString of assembly

--- src/me/mtk/torrey/X86/X86Program.java
+++ src/me/mtk/torrey/X86/X86Program.java
@@ -7,8 +7,18 @@ public final class X86Program
 {
     private List<X86Inst> instrs;
 
-    public X86Program()
+    private int stackSize;
+
+    public X86Program(int stackSize)
     {
+        // Ensure the stack pointer is 16-bytes aligned
+        // by setting this.StackSize to the nearest
+        // multiple of 16.
+        if (stackSize % 16 != 0)
+            this.stackSize = closestMultiple(stackSize, 16);
+        else
+            this.stackSize = stackSize;
+
         instrs = new ArrayList<>();
     }
 
@@ -22,15 +32,63 @@ public final class X86Program
         return instrs;
     }
 
+    // TODO: Do something better than this...
     public String toString()
     {
-        final StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder(".text\n");
 
-        for (int i = 0; i < instrs.size(); i++)
-            sb.append(instrs.get(i).toString())
-                .append(i == instrs.size() - 1 ? "" : "\n");
+        // Make the `main` procedure externally visible 
+        // so the operating system can call it. The OS
+        // will then be able to issue a `callq main`
+        // instruction, which pushes its return address
+        // on the top of our stack frame before jumping
+        // to `main`.
+        sb.append("\t.global main\n");
+        
+        // Populate the `start` procedure with the program instructions.
+        // Conclude the procedure with an unconditional control flow
+        // jump to the `conclusion` procedure.
+        sb.append("start:\n");
+        for (X86Inst inst : instrs)
+            sb.append(String.format("\t%s\n", inst));
+        sb.append("\tjmp conclusion\n");
+
+        // The entry point of our program.
+        sb.append("main:\n")
+            // Save the caller's base pointer in our stack. The base
+            // pointer points to the beginning of a stack frame. This
+            // also aligns the stack pointer.
+            .append("\tpushq %rbp\n")
+
+            // The top of the stack now contains the caller's (the OS's)
+            // base pointer (thus %rsp points to it). Change the base
+            // pointer so that it points to the location of the old
+            // base pointer.
+            .append("\tmovq %rsp, %rbp\n")
             
+            // Allocate the stack by moving the stack pointer down
+            // stackSize bytes (remember, the stack grows downward,
+            // so we must subtract to increase the stack size).
+            .append("\tsubq $").append(stackSize).append(", %rsp\n")
+
+            // Unconditionally jump to the start of our program.
+            .append("\tjmp start\n");
+
+        sb.append("conclusion:\n")
+            .append("\taddq $").append(stackSize).append(", %rsp\n")
+            .append("\tpopq %rbp\n")
+            .append("\tretq\n");
+    
         return sb.toString();
     }
+
+    private int closestMultiple(int n, int x) 
+    {    
+        if(x>n) 
+           return x; 
+        n = n + x/2; 
+        n = n - (n%x); 
+        return n; 
+    } 
 
 }
