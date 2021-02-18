c84ff38eb42c6c4ef72b80aaff46ad1a5633708f
Matthew Kosloski <matthewkosloski@me.com>
2021-02-18 03:44:20 -0600

    added error messages for parsing let expressions

--- src/main/java/me/mtk/torrey/error_reporter/ErrorMessages.java
+++ src/main/java/me/mtk/torrey/error_reporter/ErrorMessages.java
@@ -13,13 +13,17 @@ public final class ErrorMessages
 {
     public static final String UnexpectedCharacter = "Unexpected character '%s'";
     public static final String ExpectedButFound = "Expected '%s' but found '%s'";
-    public static final String ExpectedIntUnaryBinaryPrint = "Expected an integer, unary, binary, "
-        + "or print expression but found '%s' instead";
+    public static final String ExpectedExpr = "Expected an integer, unary, binary, "
+        + "print, let, or identifier expression but found '%s' instead";
     public static final String ExpectedUnaryBinaryPrint = "Expected a unary, binary, "
         + "or print expression but found '%s' instead";
     public static final String ExpectedOpeningParen = "Expected an opening parenthesis '('";
     public static final String ExpectedClosingParen = "Expected a closing parenthesis ')'";
+    public static final String ExpectedOpeningBracket = "Expected an opening bracket '['";
+    public static final String ExpectedClosingBracket = "Expected a closing bracket ']'";
     public static final String UnexpectedOperand = "Expected operand to operator '%s' to be " 
         + "type '%s' but found type '%s' instead";
     public static final String DivisionByZero = "Division by zero";
+    public static final String ExpectedIdentifier = "Expected an identifier"
+        + " but found '%s' instead";
 }
