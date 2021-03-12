af911925dd955531aae51bee92610ecb25b02403
Matthew Kosloski <matthewkosloski@me.com>
2021-03-12 09:28:04 -0600

    installed new keywords

--- src/main/java/me/mtk/torrey/Lexer/Keywords.java
+++ src/main/java/me/mtk/torrey/Lexer/Keywords.java
@@ -16,6 +16,15 @@ public final class Keywords
         store.put("print", TokenType.PRINT);
         store.put("println", TokenType.PRINTLN);
         store.put("let", TokenType.LET);
+        store.put("not", TokenType.NOT);
+        store.put("and", TokenType.AND);
+        store.put("or", TokenType.OR);
+        store.put("if", TokenType.IF);
+        store.put("==", TokenType.EQUAL);
+        store.put("<", TokenType.LT);
+        store.put("<=", TokenType.LTE);
+        store.put(">", TokenType.GT);
+        store.put(">=", TokenType.GTE);
     }
 
     /**
