602605b55dda4b3f7bbf9e78af8dc067eac0a3dd
Matthew Kosloski <matthewkosloski@me.com>
2021-03-30 14:46:35 -0500

    added BooleanExpr(boolean) constructor

--- src/main/java/me/mtk/torrey/frontend/ast/BooleanExpr.java
+++ src/main/java/me/mtk/torrey/frontend/ast/BooleanExpr.java
@@ -2,6 +2,7 @@ package me.mtk.torrey.frontend.ast;
 
 import me.mtk.torrey.frontend.analysis.DataType;
 import me.mtk.torrey.frontend.lexer.Token;
+import me.mtk.torrey.frontend.lexer.TokenType;
 
 /**
  * Represents a boolean literal expression.
@@ -11,5 +12,12 @@ public final class BooleanExpr extends PrimitiveExpr
     public BooleanExpr(Token t)
     {
         super(t, DataType.BOOLEAN);
+    }
+
+    public BooleanExpr(boolean bool)
+    {
+        this(new Token(bool 
+            ? TokenType.TRUE 
+            : TokenType.FALSE, bool + ""));
     }
 }
