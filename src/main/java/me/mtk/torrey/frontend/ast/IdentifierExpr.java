1f60b6052971907db1d9d0ea4cff5fd1142edc06
Matthew Kosloski <matthewkosloski@me.com>
2021-04-01 01:50:36 -0500

    IdentifierExpr implements EvaluatesToAnother

--- src/main/java/me/mtk/torrey/frontend/ast/IdentifierExpr.java
+++ src/main/java/me/mtk/torrey/frontend/ast/IdentifierExpr.java
@@ -3,11 +3,24 @@ package me.mtk.torrey.frontend.ast;
 import me.mtk.torrey.frontend.analysis.DataType;
 import me.mtk.torrey.frontend.lexer.Token;
 
-public class IdentifierExpr extends Expr
+public class IdentifierExpr extends Expr implements EvaluatesToAnother
 {
+
+    private Expr evaluatesTo;
+
     public IdentifierExpr(Token t) 
     {
         super(t, DataType.UNDEFINED);
+    }
+
+    public Expr evaluatesTo()
+    {
+        return evaluatesTo;
+    }
+
+    public void setEval(Expr expr)
+    {
+        evaluatesTo = expr;
     }
 
     @Override
