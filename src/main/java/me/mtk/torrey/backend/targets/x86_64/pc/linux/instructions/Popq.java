b841cfe45bf4c64de3b51a732de26cd8f5764890
Matthew Kosloski <matthewkosloski@me.com>
2021-04-18 18:24:35 -0500

    created pushq and popq x86 instruction classes

---
+++ src/main/java/me/mtk/torrey/backend/targets/x86_64/pc/linux/instructions/Popq.java
@@ -0,0 +1,11 @@
+package me.mtk.torrey.backend.targets.x86_64.pc.linux.instructions;
+
+import me.mtk.torrey.backend.targets.x86_64.pc.linux.addressing.Register;
+
+public final class Popq extends X86Inst
+{
+    public Popq(Register register)
+    {
+        super("popq", register, null);
+    }
+}
---
+++ src/main/java/me/mtk/torrey/backend/targets/x86_64/pc/linux/instructions/Pushq.java
@@ -0,0 +1,11 @@
+package me.mtk.torrey.backend.targets.x86_64.pc.linux.instructions;
+
+import me.mtk.torrey.backend.targets.x86_64.pc.linux.addressing.Register;
+
+public final class Pushq extends X86Inst
+{
+    public Pushq(Register register)
+    {
+        super("pushq", register, null);
+    }
+}
