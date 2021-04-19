92dde7997156f7658826976b397c916e384c4272
Matthew Kosloski <matthewkosloski@me.com>
2021-04-18 20:20:03 -0500

    removed generic

--- src/main/java/me/mtk/torrey/backend/targets/x86_64/pc/linux/gen/AssemblerDirective.java
+++ src/main/java/me/mtk/torrey/backend/targets/x86_64/pc/linux/gen/AssemblerDirective.java
@@ -2,12 +2,12 @@ package me.mtk.torrey.backend.targets.x86_64.pc.linux.gen;
 
 import java.util.List;
 
-public final class AssemblerDirective<T>
+public final class AssemblerDirective
 {
     private AssemblerDirectiveType type;
-    private List<T> data;
+    private List<Object> data;
 
-    public AssemblerDirective(AssemblerDirectiveType type, List<T> data)
+    public AssemblerDirective(AssemblerDirectiveType type, List<Object> data)
     {
         this.type = type;
         this.data = data;
