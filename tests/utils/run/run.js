e15e834869457f4cafd0cd9725cc0fc7b486572e
Matthew Kosloski <matthewkosloski@me.com>
2021-10-01 01:10:28 -0500

    test: removed unused function

--- tests/utils/run/run.js
+++ tests/utils/run/run.js
@@ -1,30 +1,5 @@
-const { spawn } = require('child_process');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-
-const _spawn = async (cmd, args) => {
-    return new Promise((resolve, reject) => {
-        const proc = spawn(cmd, args);
-
-        let result = {
-            stdout: Buffer.alloc(0),
-            stderr: Buffer.alloc(0),
-            exitCode: 0
-        };
-
-        proc.stdout.on('data', (buff) => result.stdout += buff);
-        proc.stderr.on('data', (buff) => result.stderr += buff);
-    
-        proc.on('close', (exitCode) => {
-            result.exitCode = exitCode;
-            resolve(result);
-        });
-
-        proc.on('error', () => {
-            reject(new Error(`Encountered an error when running ${cmd}`));
-        })
-    });
-}
 
 const run = async (program, version, args = []) => {
     program = program.trim();
