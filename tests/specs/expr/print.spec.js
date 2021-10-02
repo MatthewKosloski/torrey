9e907cbfeb061898f7fbf142402fbb8107c0890a
Matthew Kosloski <matthewkosloski@me.com>
2021-10-02 04:41:28 -0500

    test: updated print tests

--- tests/specs/expr/print.spec.js
+++ tests/specs/expr/print.spec.js
@@ -5,217 +5,181 @@ const _expect = async (program) => {
 }
 
 describe('"(" "print" expr+ ")"', () => {
-    it('Should error if no left parenthesis', async () => {
+    it('Should be a syntax error if no left parenthesis', async () => {
         const expect = await _expect(`
             print)
         `);
         expect.stderr.to.contain('Expected an integer, unary, binary, print, let, or identifier expression but found \'print\' instead');
         expect.stderr.to.contain('1 Error');
     });
-    it('Should error if no child expressions', async () => {
+    it('Should be a syntax error if no operands', async () => {
         const expect = await _expect(`
             (print)
         `);
         expect.stderr.to.contain('Expected an integer, unary, binary, print, let, or identifier expression but found \')\' instead');
         expect.stderr.to.contain('1 Error');
     });
-    it('Should error if no right parenthesis', async () => {
+    it('Should be a syntax error if no right parenthesis', async () => {
         const expect = await _expect(`
             (print
         `);
         expect.stderr.to.contain('Expected an integer, unary, binary, print, let, or identifier expression but found \'print\' instead');
         expect.stderr.to.contain('1 Error');
     });
-    it('Should print an integer literal expression', async () => {
+    it('Should accept an integer literal operand', async () => {
         const expect = await _expect(`
             (print 3)
         `);
         expect.stdout.to.equal('3');
     });
-    it('Should print more than one integer literal expression', async () => {
+    it('Should accept more than one integer literal operands', async () => {
         const expect = await _expect(`
             (print 1 2 3)
         `);
         expect.stdout.to.equal('123');
     });
-    // TODO: Should be able to print boolean literal true
-    // TODO: Should be able to print more than one boolean literal true
-    // TODO: Should be able to print boolean literal false
-    // TODO: Should be able to print more than one boolean literal false
-    it('Should print an identifier expression', async () => {
+    // TODO: Should accept a boolean literal true operand
+    // TODO: Should accept more than one boolean literal true operands
+    // TODO: Should accept a boolean literal false operand
+    // TODO: Should accept more than one boolean literal false operands
+    it('Should accept an identifier operand', async () => {
         const expect = await _expect(`
             (let [a 4]
                 (print a))
         `);
         expect.stdout.to.equal('4');
     });
-    it('Should print more than one identifier expression', async () => {
+    it('Should accept more than one identifier operands', async () => {
         const expect = await _expect(`
             (let [a 4 b 100]
-                (print a a b))
+                (print a b))
         `);
-        expect.stdout.to.equal('44100');
+        expect.stdout.to.equal('4100');
     });
-    it('Should print a unary expression', async () => {
+    it('Should accept a unary operand', async () => {
         const expect = await _expect(`
             (print (- 1))
         `);
         expect.stdout.to.equal('-1');
     });
-    it('Should print more than one unary expression', async () => {
+    it('Should accept more than one unary operands', async () => {
         const expect = await _expect(`
             (print (- 1) (- 10) (- 999))
         `);
         expect.stdout.to.equal('-1-10-999');
     });
-    it('Should print a binary addition expression', async () => {
+    it('Should accept a binary addition operand', async () => {
         const expect = await _expect(`
             (print (+ 2 3))
         `);
         expect.stdout.to.equal('5');
     });
-    it('Should print more than one binary addition expression', async () => {
+    it('Should accept more than one binary addition operands', async () => {
         const expect = await _expect(`
             (print (+ 1 3) (+ 2 (- 3)))
         `);
         expect.stdout.to.equal('4-1');
     });
-    it('Should print a binary subtraction expression', async () => {
+    it('Should accept a binary subtraction operand', async () => {
         const expect = await _expect(`
             (print (- 2 3))
         `);
         expect.stdout.to.equal('-1');
     });
-    it('Should print more than one binary subtraction expression', async () => {
+    it('Should accept more than one binary subtraction operands', async () => {
         const expect = await _expect(`
             (print (- 2 3) (- 5 3))
         `);
         expect.stdout.to.equal('-12');
     });
-    it('Should print a binary multiplication expression', async () => {
+    it('Should accept a binary multiplication operand', async () => {
         const expect = await _expect(`
             (print (* 100 (- 3)))
         `);
         expect.stdout.to.equal('-300');
     });
-    it('Should print more than one binary multiplication expression', async () => {
+    it('Should accept more than one binary multiplication operands', async () => {
         const expect = await _expect(`
             (print (* 5 (- 9)) (* 8 3))
         `);
         expect.stdout.to.equal('-4524');
     });
-    it('Should print a binary division expression', async () => {
+    it('Should accept a binary division operand', async () => {
         const expect = await _expect(`
             (print (/ 5 2))
         `);
         expect.stdout.to.equal('2');
     });
-    it('Should print more than one binary division expression', async () => {
+    it('Should accept more than one binary division operands', async () => {
         const expect = await _expect(`
             (print (/ 2 20) (/ (- 16) 4))
         `);
         expect.stdout.to.equal('0-4');
     });
-    // TODO: Should be able to print ==
-    // TODO: Should be able to print more than one ==
-    // TODO: Should be able to print <
-    // TODO: Should be able to print more than one <
-    // TODO: Should be able to print <=
-    // TODO: Should be able to print more than one <=
-    // TODO: Should be able to print >
-    // TODO: Should be able to print more than one >
-    // TODO: Should be able to print >=
-    // TODO: Should be able to print more than one >=
-    it('Should not be able to print a print expression', async () => {
+    // TODO: Should accept ==
+    // TODO: Should accept more than one ==
+    // TODO: Should accept <
+    // TODO: Should accept more than one <
+    // TODO: Should accept <=
+    // TODO: Should accept more than one <=
+    // TODO: Should accept >
+    // TODO: Should accept more than one >
+    // TODO: Should accept >=
+    // TODO: Should accept more than one >=
+    it('Should be a semantic error if operand is a print expression', async () => {
         const expect = await _expect(`
             (print (print 42))
         `);
         expect.stderr.to.contain('Cannot print operand \'print\' because it does not evaluate to a known type');
         expect.stderr.to.contain('1 Error');
     });
-    it('Should not be able to print more than one print expression', async () => {
+    it('Should be a semantic error if operands are print expressions', async () => {
         const expect = await _expect(`
             (print (print 42) (print (- 42)))
         `);
         expect.stderr.to.contain('Cannot print operand \'print\' because it does not evaluate to a known type');
         expect.stderr.to.contain('2 Errors');
     });
-    it('Should not be able to print an empty let expression', async () => {
-        const expect = await _expect(`
-            (print (let []))
-        `);
-        expect.stderr.to.contain('Cannot print operand \'let\' because it does not evaluate to a known type');
-        expect.stderr.to.contain('1 Error');
-    });
-    it('Should not be able to print more than one empty let expression', async () => {
-        const expect = await _expect(`
-            (print (let []) (let []))
-        `);
-        expect.stderr.to.contain('Cannot print operand \'let\' because it does not evaluate to a known type');
-        expect.stderr.to.contain('2 Errors');
-    });
-    it('Should not be able to print let expressions that evaluate to nil', async () => {
-        const expect = await _expect(`
-            (print (let [] (let [])) (let [] (print 42)))
-        `);
-        expect.stderr.to.contain('Cannot print operand \'let\' because it does not evaluate to a known type');
-        expect.stderr.to.contain('2 Errors');
-    });
-    it('Should print a let expression that evaluates to an integer', async () => {
+    it('Should accept a let expression that evaluates to a non-nil expression', async () => {
         const expect = await _expect(`
             (print (let [] 42))
         `);
         expect.stdout.to.equal('42');
     });
-    it('Should print more than one let expression that evaluates to an integer', async () => {
+    it('Should be a semantic error if operand is a let expression that evaluates to a nil expression', async () => {
         const expect = await _expect(`
-            (print (let [] 69) (let [] 420))
+            (print (let [] (print 42)))
+        `);
+        expect.stderr.to.contain('Cannot print operand \'let\' because it does not evaluate to a known type');
+        expect.stderr.to.contain('1 Error');
+    });
+    // TODO: Should accept not
+    // TODO: Should accept and
+    // TODO: Should accept or
+    it('Should accept an if-then expression that evaluates to a non-nil expression', async () => {
+        const expect = await _expect(`
+            (print (if true 69420))
         `);
         expect.stdout.to.equal('69420');
     });
-    // TODO: Should print a let expression that evaluates to a boolean literal true
-    // TODO: Should print a let expression that evaluates to more than one boolean literal true
-    // TODO: Should print a let expression that evaluates to a boolean literal false
-    // TODO: Should print a let expression that evaluates to more than one boolean literal false
-    it('Should print a let expression that evaluates to an identifier', async () => {
+    it('Should be a semantic error if operand is an if-then expression that evaluates to a nil expression', async () => {
         const expect = await _expect(`
-            (print (let [a 5] a))
+            (print (if true (let [])))
         `);
-        expect.stdout.to.equal('5');
+        expect.stderr.to.contain('Cannot print operand \'if\' because it does not evaluate to a known type');
+        expect.stderr.to.contain('1 Error');
     });
-    it('Should print a let expression that evaluates to a unary expression', async () => {
+    it('Should accept an if-then-else expression that evaluates to a non-nil expression', async () => {
         const expect = await _expect(`
-            (print (let [] (- 3)))
+            (print (if false 0 1))
         `);
-        expect.stdout.to.equal('-3');
+        expect.stdout.to.equal('1');
     });
-    it('Should print a let expression that evaluates to a binary addition expression', async () => {
+    it('Should be a semantic error if operand is an if-then-else expression that evaluates to a nil expression', async () => {
         const expect = await _expect(`
-            (print (let [] (+ 2 2)))
+            (print (if false 696969 (let [])))
         `);
-        expect.stdout.to.equal('4');
+        expect.stderr.to.contain('Cannot print operand \'if\' because it does not evaluate to a known type');
+        expect.stderr.to.contain('1 Error');
     });
-    it('Should print a let expression that evaluates to a binary subtraction expression', async () => {
-        const expect = await _expect(`
-            (print (let [] (- 3 5)))
-        `);
-        expect.stdout.to.equal('-2');
-    });
-    it('Should print a let expression that evaluates to a binary multiplication expression', async () => {
-        const expect = await _expect(`
-            (print (let [] (* 1 6)))
-        `);
-        expect.stdout.to.equal('6');
-    });
-    it('Should print a let expression that evaluates to a binary division expression', async () => {
-        const expect = await _expect(`
-            (print (let [] (/ 7 3)))
-        `);
-        expect.stdout.to.equal('2');
-    });
-    // TODO: Should print a let expression that evaluates ==
-    // TODO: Should print a let expression that evaluates <
-    // TODO: Should print a let expression that evaluates <=
-    // TODO: Should print a let expression that evaluates >
-    // TODO: Should print a let expression that evaluates >=
 });
--- tests/specs/expr/println.spec.js
+++ tests/specs/expr/println.spec.js
@@ -5,217 +5,181 @@ const _expect = async (program) => {
 }
 
 describe('"(" "println" expr+ ")"', () => {
-    it('Should error if no left parenthesis', async () => {
+    it('Should be a syntax error if no left parenthesis', async () => {
         const expect = await _expect(`
             println)
         `);
         expect.stderr.to.contain('Expected an integer, unary, binary, print, let, or identifier expression but found \'println\' instead');
         expect.stderr.to.contain('1 Error');
     });
-    it('Should error if no child expressions', async () => {
+    it('Should be a syntax error if no operands', async () => {
         const expect = await _expect(`
             (println)
         `);
         expect.stderr.to.contain('Expected an integer, unary, binary, print, let, or identifier expression but found \')\' instead');
         expect.stderr.to.contain('1 Error');
     });
-    it('Should error if no right parenthesis', async () => {
+    it('Should be a syntax error if no right parenthesis', async () => {
         const expect = await _expect(`
             (println
         `);
         expect.stderr.to.contain('Expected an integer, unary, binary, print, let, or identifier expression but found \'println\' instead');
         expect.stderr.to.contain('1 Error');
     });
-    it('Should print an integer literal expression', async () => {
+    it('Should accept an integer literal operand', async () => {
         const expect = await _expect(`
             (println 3)
         `);
         expect.stdout.to.equal('3\n');
     });
-    it('Should print more than one integer literal expression', async () => {
+    it('Should accept more than one integer literal operands', async () => {
         const expect = await _expect(`
             (println 1 2 3)
         `);
         expect.stdout.to.equal('1\n2\n3\n');
     });
-    // TODO: Should be able to print boolean literal true
-    // TODO: Should be able to print more than one boolean literal true
-    // TODO: Should be able to print boolean literal false
-    // TODO: Should be able to print more than one boolean literal false
-    it('Should print an identifier expression', async () => {
+    // TODO: Should accept a boolean literal true operand
+    // TODO: Should accept more than one boolean literal true operands
+    // TODO: Should accept a boolean literal false operand
+    // TODO: Should accept more than one boolean literal false operands
+    it('Should accept an identifier operand', async () => {
         const expect = await _expect(`
             (let [a 4]
                 (println a))
         `);
         expect.stdout.to.equal('4\n');
     });
-    it('Should print more than one identifier expression', async () => {
+    it('Should accept more than one identifier operands', async () => {
         const expect = await _expect(`
             (let [a 4 b 100]
-                (println a a b))
+                (println a b))
         `);
-        expect.stdout.to.equal('4\n4\n100\n');
+        expect.stdout.to.equal('4\n100\n');
     });
-    it('Should print a unary expression', async () => {
+    it('Should accept a unary operand', async () => {
         const expect = await _expect(`
             (println (- 1))
         `);
         expect.stdout.to.equal('-1\n');
     });
-    it('Should print more than one unary expression', async () => {
+    it('Should accept more than one unary operands', async () => {
         const expect = await _expect(`
             (println (- 1) (- 10) (- 999))
         `);
         expect.stdout.to.equal('-1\n-10\n-999\n');
     });
-    it('Should print a binary addition expression', async () => {
+    it('Should accept a binary addition operand', async () => {
         const expect = await _expect(`
             (println (+ 2 3))
         `);
         expect.stdout.to.equal('5\n');
     });
-    it('Should print more than one binary addition expression', async () => {
+    it('Should accept more than one binary addition operands', async () => {
         const expect = await _expect(`
             (println (+ 1 3) (+ 2 (- 3)))
         `);
         expect.stdout.to.equal('4\n-1\n');
     });
-    it('Should print a binary subtraction expression', async () => {
+    it('Should accept a binary subtraction operand', async () => {
         const expect = await _expect(`
             (println (- 2 3))
         `);
         expect.stdout.to.equal('-1\n');
     });
-    it('Should print more than one binary subtraction expression', async () => {
+    it('Should accept more than one binary subtraction operands', async () => {
         const expect = await _expect(`
             (println (- 2 3) (- 5 3))
         `);
         expect.stdout.to.equal('-1\n2\n');
     });
-    it('Should print a binary multiplication expression', async () => {
+    it('Should accept a binary multiplication operand', async () => {
         const expect = await _expect(`
             (println (* 100 (- 3)))
         `);
         expect.stdout.to.equal('-300\n');
     });
-    it('Should print more than one binary multiplication expression', async () => {
+    it('Should accept more than one binary multiplication operands', async () => {
         const expect = await _expect(`
             (println (* 5 (- 9)) (* 8 3))
         `);
         expect.stdout.to.equal('-45\n24\n');
     });
-    it('Should print a binary division expression', async () => {
+    it('Should accept a binary division operand', async () => {
         const expect = await _expect(`
             (println (/ 5 2))
         `);
         expect.stdout.to.equal('2\n');
     });
-    it('Should print more than one binary division expression', async () => {
+    it('Should accept more than one binary division operands', async () => {
         const expect = await _expect(`
             (println (/ 2 20) (/ (- 16) 4))
         `);
         expect.stdout.to.equal('0\n-4\n');
     });
-    // TODO: Should be able to print ==
-    // TODO: Should be able to print more than one ==
-    // TODO: Should be able to print <
-    // TODO: Should be able to print more than one <
-    // TODO: Should be able to print <=
-    // TODO: Should be able to print more than one <=
-    // TODO: Should be able to print >
-    // TODO: Should be able to print more than one >
-    // TODO: Should be able to print >=
-    // TODO: Should be able to print more than one >=
-    it('Should not be able to print a print expression', async () => {
+    // TODO: Should accept ==
+    // TODO: Should accept more than one ==
+    // TODO: Should accept <
+    // TODO: Should accept more than one <
+    // TODO: Should accept <=
+    // TODO: Should accept more than one <=
+    // TODO: Should accept >
+    // TODO: Should accept more than one >
+    // TODO: Should accept >=
+    // TODO: Should accept more than one >=
+    it('Should be a semantic error if operand is a print expression', async () => {
         const expect = await _expect(`
             (println (println 42))
         `);
         expect.stderr.to.contain('Cannot print operand \'println\' because it does not evaluate to a known type');
         expect.stderr.to.contain('1 Error');
     });
-    it('Should not be able to print more than one print expression', async () => {
+    it('Should be a semantic error if operands are print expressions', async () => {
         const expect = await _expect(`
             (println (println 42) (println (- 42)))
         `);
         expect.stderr.to.contain('Cannot print operand \'println\' because it does not evaluate to a known type');
         expect.stderr.to.contain('2 Errors');
     });
-    it('Should not be able to print an empty let expression', async () => {
-        const expect = await _expect(`
-            (println (let []))
-        `);
-        expect.stderr.to.contain('Cannot print operand \'let\' because it does not evaluate to a known type');
-        expect.stderr.to.contain('1 Error');
-    });
-    it('Should not be able to print more than one empty let expression', async () => {
-        const expect = await _expect(`
-            (println (let []) (let []))
-        `);
-        expect.stderr.to.contain('Cannot print operand \'let\' because it does not evaluate to a known type');
-        expect.stderr.to.contain('2 Errors');
-    });
-    it('Should not be able to print let expressions that evaluate to nil', async () => {
-        const expect = await _expect(`
-            (println (let [] (let [])) (let [] (println 42)))
-        `);
-        expect.stderr.to.contain('Cannot print operand \'let\' because it does not evaluate to a known type');
-        expect.stderr.to.contain('2 Errors');
-    });
-    it('Should print a let expression that evaluates to an integer', async () => {
+    it('Should accept a let expression that evaluates to a non-nil expression', async () => {
         const expect = await _expect(`
             (println (let [] 42))
         `);
         expect.stdout.to.equal('42\n');
     });
-    it('Should print more than one let expression that evaluates to an integer', async () => {
+    it('Should be a semantic error if operand is a let expression that evaluates to a nil expression', async () => {
         const expect = await _expect(`
-            (println (let [] 69) (let [] 420))
+            (println (let [] (println 42)))
         `);
-        expect.stdout.to.equal('69\n420\n');
+        expect.stderr.to.contain('Cannot print operand \'let\' because it does not evaluate to a known type');
+        expect.stderr.to.contain('1 Error');
     });
-    // TODO: Should print a let expression that evaluates to a boolean literal true
-    // TODO: Should print a let expression that evaluates to more than one boolean literal true
-    // TODO: Should print a let expression that evaluates to a boolean literal false
-    // TODO: Should print a let expression that evaluates to more than one boolean literal false
-    it('Should print a let expression that evaluates to an identifier', async () => {
+    // TODO: Should accept not
+    // TODO: Should accept and
+    // TODO: Should accept or
+    it('Should accept an if-then expression that evaluates to a non-nil expression', async () => {
         const expect = await _expect(`
-            (println (let [a 5] a))
+            (println (if true 69420))
         `);
-        expect.stdout.to.equal('5\n');
+        expect.stdout.to.equal('69420\n');
     });
-    it('Should print a let expression that evaluates to a unary expression', async () => {
+    it('Should be a semantic error if operand is an if-then expression that evaluates to a nil expression', async () => {
         const expect = await _expect(`
-            (println (let [] (- 3)))
+            (println (if true (let [])))
         `);
-        expect.stdout.to.equal('-3\n');
+        expect.stderr.to.contain('Cannot print operand \'if\' because it does not evaluate to a known type');
+        expect.stderr.to.contain('1 Error');
     });
-    it('Should print a let expression that evaluates to a binary addition expression', async () => {
+    it('Should accept an if-then-else expression that evaluates to a non-nil expression', async () => {
         const expect = await _expect(`
-            (println (let [] (+ 2 2)))
+            (println (if false 0 1))
         `);
-        expect.stdout.to.equal('4\n');
+        expect.stdout.to.equal('1\n');
     });
-    it('Should print a let expression that evaluates to a binary subtraction expression', async () => {
+    it('Should be a semantic error if operand is an if-then-else expression that evaluates to a nil expression', async () => {
         const expect = await _expect(`
-            (println (let [] (- 3 5)))
+            (println (if false 696969 (let [])))
         `);
-        expect.stdout.to.equal('-2\n');
+        expect.stderr.to.contain('Cannot print operand \'if\' because it does not evaluate to a known type');
+        expect.stderr.to.contain('1 Error');
     });
-    it('Should print a let expression that evaluates to a binary multiplication expression', async () => {
-        const expect = await _expect(`
-            (println (let [] (* 1 6)))
-        `);
-        expect.stdout.to.equal('6\n');
-    });
-    it('Should print a let expression that evaluates to a binary division expression', async () => {
-        const expect = await _expect(`
-            (println (let [] (/ 7 3)))
-        `);
-        expect.stdout.to.equal('2\n');
-    });
-    // TODO: Should print a let expression that evaluates ==
-    // TODO: Should print a let expression that evaluates <
-    // TODO: Should print a let expression that evaluates <=
-    // TODO: Should print a let expression that evaluates >
-    // TODO: Should print a let expression that evaluates >=
 });
