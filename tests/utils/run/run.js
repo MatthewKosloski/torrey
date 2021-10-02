f24de9beb5639d44fa7289473d283ead7834033e
Matthew Kosloski <matthewkosloski@me.com>
2021-10-02 04:42:05 -0500

    chore: allow empty program and don't remove its leading and trailing ws

--- tests/utils/run/run.js
+++ tests/utils/run/run.js
@@ -2,13 +2,12 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 const run = async (program, version, args = []) => {
-    program = program.trim();
     version = version.trim();
-    if (program && version) {
+    if (version) {
         const pathPrefix = './utils/run';
         return await exec(`bash ${pathPrefix}/run.sh "${program}" ${version} ${pathPrefix} ${args.join(' ')}`);
     }
-    throw new Error('Need a program and path to compiler to run.');
+    throw new Error('Must specify compiler version to run.');
 };
 
 module.exports = run;
