4273968812dd06225574559c36baa2e22adec512
Matthew Kosloski <matthewkosloski@me.com>
2021-10-03 21:38:59 -0500

    chore: added maven to provision file

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,6 @@ apt-get update -y && apt-get upgrade -y
 
 # Install Java JDK and gcc, g++, and make
 apt-get install -y default-jdk build-essential
+
+# Install Maven (used to build the compiler for tests)
+apt-get install maven -y
