cdf93bfe7d9083dc0fa19a40ccd9299d0af22f14
Matthew Kosloski <matthewkosloski@me.com>
2021-12-20 02:24:06 -0600

    chore: list directory

--- prepare-semantic-release.sh
+++ prepare-semantic-release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "starting to execute prepare-semantic-release with next version $1"
+echo "listing directory"
+ls
 mvn --batch-mode --no-transfer-progress versions:set -DgenerateBackupPoms=false -DnewVersion=$1 \
     && mvn --batch-mode --no-transfer-progress --update-snapshots package
