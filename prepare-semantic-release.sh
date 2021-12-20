0aafdaebf1471f91b7396c81c1d52d2f3be460a9
Matthew Kosloski <matthewkosloski@me.com>
2021-12-20 02:12:22 -0600

    chore: run maven in batch mode

--- prepare-semantic-release.sh
+++ prepare-semantic-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "starting to execute prepare-semantic-release with next version $1"
-mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1 \
-    && mvn --update-snapshots package
+mvn --batch-mode versions:set -DgenerateBackupPoms=false -DnewVersion=$1 \
+    && mvn --batch-mode --update-snapshots package
