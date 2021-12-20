f8911ece93edfdd9fb5e8bcec4fbda4c1970ca95
Matthew Kosloski <matthewkosloski@me.com>
2021-12-20 02:06:57 -0600

    chore: update prepare script

--- prepare-semantic-release.sh
+++ prepare-semantic-release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "starting to execute prepare-semantic-release with next version $1"
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1 \
     && mvn --update-snapshots package
