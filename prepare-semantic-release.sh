776c17159491864e7f1584b54154edb8b2dda920
Matthew Kosloski <matthewkosloski@me.com>
2021-12-20 02:15:31 -0600

    chore: hide maven download progress

--- prepare-semantic-release.sh
+++ prepare-semantic-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "starting to execute prepare-semantic-release with next version $1"
 mvn --batch-mode versions:set -DgenerateBackupPoms=false -DnewVersion=$1 \
-    && mvn --batch-mode --update-snapshots package
+    && mvn --batch-mode --no-transfer-progress --update-snapshots package
