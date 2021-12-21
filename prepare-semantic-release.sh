cef5be262c77f098c2167d0af818cf1dc122f9e5
Matthew Kosloski <matthewkosloski@me.com>
2021-12-20 22:02:06 -0600

    chore: create release.zip

--- prepare-semantic-release.sh
+++ prepare-semantic-release.sh
@@ -6,4 +6,5 @@ ls \
 	&& mvn --batch-mode --update-snapshots --no-transfer-progress package \
 	&& gcc -c ./src/runtime/runtime.c -o runtime.o \
 	&& mkdir release \
-	&& mv ./runtime.o ./target/torreyc-$1.jar ./release
+	&& mv ./runtime.o ./target/torreyc-$1.jar ./release \
+	&& zip -r release.zip release
