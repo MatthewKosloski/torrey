48ae3af5508ac194dd0d06b4ed4d8645f4a7bcf1
Matthew Kosloski <matthewkosloski@me.com>
2022-04-16 00:24:02 -0500

    test: refactored unary tests to use new APIs

--- e2e/unary_expr_tests.sh
+++ e2e/unary_expr_tests.sh
@@ -5,9 +5,9 @@ source _utils.sh
 run_unary_expr_tests () {
   echo "Tests for \"(\" \"-\" expr \")\""
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no left parenthesis and no operand" \
+    $1 \
     "-)" \
     "Encountered one or more syntax errors during parse:
 
@@ -19,9 +19,9 @@ Expected an integer, unary, binary, print, let, or identifier expression but fou
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no left parenthesis" \
+    $1 \
     "- 69)" \
     "Encountered one or more syntax errors during parse:
 
@@ -33,9 +33,9 @@ Expected an integer, unary, binary, print, let, or identifier expression but fou
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no operand" \
+    $1 \
     "(-)" \
     "Encountered one or more syntax errors during parse:
 
@@ -47,9 +47,9 @@ Expected an integer, unary, binary, print, let, or identifier expression but fou
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no right parenthesis and no operand" \
+    $1 \
     "(-" \
     "Encountered one or more syntax errors during parse:
 
@@ -61,9 +61,9 @@ Expected an integer, unary, binary, print, let, or identifier expression but fou
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no right parenthesis" \
+    $1 \
     "(- 420" \
     "Encountered one or more syntax errors during parse:
 
@@ -75,15 +75,15 @@ Expected a closing parenthesis ')' (1:4)
 
 1 Error"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should evaluate to zero if the operand is literal 0" \
+    $1 \
     "(print (- 0))" \
     "0"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is literal true" \
+    $1 \
     "(- true)" \
     "Encountered one or more syntax errors during parse:
 
@@ -95,9 +95,9 @@ Expected a closing parenthesis ')' (1:4)
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is literal false" \
+    $1 \
     "(- false)" \
     "Encountered one or more syntax errors during parse:
 
@@ -109,30 +109,30 @@ Expected a closing parenthesis ')' (1:4)
 
 1 Error"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should evaluate to a positive integer if the operand is an integer literal nested within a unary expression" \
+    $1 \
     "(println
        (- (- 0))
        (- (- 1)))" \
     "0
 1"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should evaluate to a negative integer if the operand is an integer literal nested within a double unary expression" \
+    $1 \
     "(print (- (- (- 25))))" \
     "-25"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should evaluate to zero if the operand is 0 nested in a double unary expression" \
+    $1 \
     "(print (- (- (- 0))))" \
     "0"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should evaluate unary expressions of arbitrary depth" \
+    $1 \
     "(println (- (- (- (- (- (- (- 3141519))))))))
      (println (- (- (- (- (- (- (- (- (- (- (- (- 6942069)))))))))))))
      (println (- (- (- (- (- (- (- (- (- (- (- (- (- (- (- (- (- (- (- 99999999))))))))))))))))))))" \
@@ -140,32 +140,32 @@ Expected a closing parenthesis ')' (1:4)
 6942069
 -99999999"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept an integer literal operand" \
+    $1 \
     "(print
        (- 42))" \
     "-42"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept an identifier operand" \
+    $1 \
     "(print
        (let [a 400]
          (- a)))" \
     "-400"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a binary addition expression operand" \
+    $1 \
     "(print
        (-
          (+ 3 (- 5))))" \
     "2"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a binary subtraction expression operand" \
+    $1 \
     "(print
        (-
          (-
@@ -173,102 +173,102 @@ Expected a closing parenthesis ')' (1:4)
            69)))" \
     "-351"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a binary multiplication expression operand" \
+    $1 \
     "(print (- (* 9 0)))" \
     "0"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a binary division expression operand" \
+    $1 \
     "(print
        (- (/ 12 (- 5))))" \
     "2"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is a print expression" \
+    $1 \
     "(print
        (- (print 42)))" \
     ""
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is a println expression" \
+    $1 \
     "(print
        (- (println 42)))" \
     ""
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a let expression that evaluates to an integer" \
+    $1 \
     "(print
        (- (let [] 42)))" \
     "-42"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is a let expression that does not evaluate to an integer" \
+    $1 \
     "(- (let [] true))" \
     ""
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept an if-then expression that evaluates to an integer" \
+    $1 \
     "(print
        (- (if true 69420)))" \
     "-69420"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is an if-then expression that does not evaluate to an integer" \
+    $1 \
     "(- (if true (== 2 3)))" \
     ""
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept an if-then-else expression that evaluates to an integer" \
+    $1 \
     "(print
        (- (if false 0 1)))" \
     "-1"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is an if-then-else expression that does not evaluate to an integer" \
+    $1 \
     "(- (if
          false
          (>= 1 2)
          (< 5 6)))" \
     ""
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is a relational equal to expression" \
+    $1 \
     "(- (== 32 10))" \
     ""
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is a relational less than expression" \
+    $1 \
     "(- (< 32 10))" \
     ""
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is a relational less than or equal to expression" \
+    $1 \
     "(- (<= 32 10))" \
     ""
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is a relational greater than expression" \
+    $1 \
     "(- (> 32 10))" \
     ""
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a type error if the operand is a relational greater than or equal to expression" \
+    $1 \
     "(- (>= 32 10))" \
     ""
 
