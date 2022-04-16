657dd5fd70b6a00c84e8ae8762a6affda5d7d304
Matthew Kosloski <matthewkosloski@me.com>
2022-04-16 00:17:42 -0500

    test: refactored print tests to use new APIs

--- e2e/print_expr_tests.sh
+++ e2e/print_expr_tests.sh
@@ -5,9 +5,9 @@ source _utils.sh
 run_print_expr_tests () {
   echo "Tests for \"(\" \"print\" expr+ \")\""
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no left parenthesis and no operand" \
+    $1 \
     "print)" \
     "Encountered one or more syntax errors during parse:
 
@@ -19,9 +19,9 @@ print)
 
 1 Error" \
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no left parenthesis" \
+    $1 \
     "print 69)" \
     "Encountered one or more syntax errors during parse:
 
@@ -33,9 +33,9 @@ print 69)
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no operand" \
+    $1 \
     "(print)" \
     "Encountered one or more syntax errors during parse:
 
@@ -47,9 +47,9 @@ Expected an integer, unary, binary, print, let, or identifier expression but fou
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no right parenthesis and no operand" \
+    $1 \
     "(print" \
     "Encountered one or more syntax errors during parse:
 
@@ -61,9 +61,9 @@ Expected an integer, unary, binary, print, let, or identifier expression but fou
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a syntax error if no right parenthesis" \
+    $1 \
     "(print 420" \
     "Encountered one or more syntax errors during parse:
 
@@ -75,95 +75,95 @@ Expected a closing parenthesis ')' (1:8)
 
 1 Error"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept an integer literal operand" \
+    $1 \
     "(print 3)" \
     "3"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept more than one integer literal operands" \
+    $1 \
     "(print 1 2 3)" \
     "123"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept an identifier operand" \
+    $1 \
     "(let [a 4]
        (print a))" \
     "4"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept more than one identifier operands" \
+    $1 \
     "(let [a 4 b 100]
        (print a b))" \
     "4100"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a unary operand" \
+    $1 \
     "(print (- 1))" \
     "-1"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept more than one unary operands" \
+    $1 \
     "(print (- 1) (- 10) (- 999))" \
     "-1-10-999"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a binary addition operand" \
+    $1 \
     "(print (+ 2 3))" \
     "5"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept more than one binary addition operands" \
+    $1 \
     "(print (+ 1 3) (+ 2 (- 3)))" \
     "4-1"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a binary subtraction operand" \
+    $1 \
     "(print (- 2 3))" \
     "-1"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept more than one binary subtraction operands" \
+    $1 \
     "(print (- 2 3) (- 5 3))" \
     "-12"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a binary multiplication operand" \
+    $1 \
     "(print (* 100 (- 3)))" \
     "-300"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept more than one binary multiplication operands" \
+    $1 \
     "(print (* 5 (- 9)) (* 8 3))" \
     "-4524"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a binary division operand" \
+    $1 \
     "(print (/ 5 2))" \
     "2"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept more than one binary division operands" \
+    $1 \
     "(print (/ 2 20) (/ (- 16) 4))" \
     "0-4"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a semantic error if the operand is a print expression" \
+    $1 \
     "(print (print 42))" \
     "Encountered one or more semantic errors during type checking:
 
@@ -175,9 +175,9 @@ Cannot print operand 'print' because it does not evaluate to a known type (1:9)
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a semantic error if the operands are print expressions" \
+    $1 \
     "(print (print 42) (print (- 42)))" \
     "Encountered one or more semantic errors during type checking:
 
@@ -194,9 +194,9 @@ Cannot print operand 'print' because it does not evaluate to a known type (1:20)
 
 2 Errors"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a semantic error if the operand is a println expression" \
+    $1 \
     "(print (println 42))" \
     "Encountered one or more semantic errors during type checking:
 
@@ -208,9 +208,9 @@ Cannot print operand 'println' because it does not evaluate to a known type (1:9
 
 1 Error"
 
-  assert_stderr \
-    $1 \
+  assert_torreyc_stderr_equalto_with_stdin \
     "Should report a semantic error if the operands are println expressions" \
+    $1 \
     "(print (println 42) (println (- 42)))" \
     "Encountered one or more semantic errors during type checking:
 
@@ -227,21 +227,21 @@ Cannot print operand 'println' because it does not evaluate to a known type (1:2
 
 2 Errors"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept a let expression" \
+    $1 \
     "(print (let [] 42))" \
     "42"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept an if-then expression" \
+    $1 \
     "(print (if true 69420))" \
     "69420"
 
-  assert_stdout \
-    $1 \
+  assert_exec_stdout_equalto_with_stdin \
     "Should accept an if-then-else expression" \
+    $1 \
     "(print (if false 0 1))" \
     "1"
 
