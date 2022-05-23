b145429345ca894298fa9730f28f37f78c1fb309
Matthew Kosloski <matthewkosloski@me.com>
2022-05-23 01:51:20 -0500

    fix: remove ConstantConvertable impl from UnaryExpr

--- src/main/java/me/mtk/torrey/frontend/ast/UnaryExpr.java
+++ src/main/java/me/mtk/torrey/frontend/ast/UnaryExpr.java
@@ -2,7 +2,7 @@ package me.mtk.torrey.frontend.ast;
 
 import me.mtk.torrey.frontend.lexer.Token;
 
-public final class UnaryExpr extends Expr implements ConstantConvertable
+public final class UnaryExpr extends Expr
 {
   public UnaryExpr(Token unaryOp, Expr operand)
   {
@@ -16,11 +16,5 @@ public final class UnaryExpr extends Expr implements ConstantConvertable
   public <T> T accept(ASTNodeVisitor<T> visitor)
   {
     return visitor.visit(this);
-  }
-
-  public long toConstant()
-  {
-    final String rawText = first().token().rawText();
-    return Long.parseLong(rawText) * -1;
   }
 }
