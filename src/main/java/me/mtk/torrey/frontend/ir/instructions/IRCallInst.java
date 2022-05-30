769be0189da80e580e7d2c029e1c394691408898
Matthew Kosloski <matthewkosloski@me.com>
2022-05-29 19:43:16 -0500

    fix: removed unused IRCallInst constructor

--- src/main/java/me/mtk/torrey/frontend/ir/instructions/IRCallInst.java
+++ src/main/java/me/mtk/torrey/frontend/ir/instructions/IRCallInst.java
@@ -5,26 +5,7 @@ import me.mtk.torrey.frontend.ir.addressing.*;
 public final class IRCallInst extends Quadruple
 {
   /**
-   * Instantiates a procedure call IR instruction,
-   * storing the result of the procedure in the
-   * specified result address.
-   *
-   * @param result The temp address at which the result of the
-   * procedure is to be stored.
-   * @param procName A name address containing the name of
-   * the procedure.
-   * @param numParams A constant address indicating the number
-   * of parameters to the procedure.
-   */
-  public IRCallInst(IRTempAddress result, IRNameAddress procName,
-      IRConstAddress numParams)
-  {
-    super(OpType.CALL, procName, numParams, result);
-  }
-
-  /**
-   * Instantiates a procedure call IR instruction
-   * without a result address.
+   * Instantiates a procedure call IR instruction.
    *
    * @param procName An name address containing the name of
    * the procedure.
@@ -33,7 +14,7 @@ public final class IRCallInst extends Quadruple
    */
   public IRCallInst(IRNameAddress procName, IRConstAddress numParams)
   {
-    this(null, procName, numParams);
+    super(OpType.CALL, procName, numParams);
   }
 
   /**
